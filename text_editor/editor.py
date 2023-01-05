a9b4ece7f1f8787ef329b7c81c3359b6c682dbbd
Pittchai <pittchai@base32>
2023-01-05 23:23:46 +0300

    flake-format

--- text_editor/editor.py
+++ text_editor/editor.py
@@ -1,12 +1,13 @@
 from tkinter import Frame, Menu, Text, Scrollbar, filedialog, messagebox, Tk
 
+
 class TextEditor(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.up_menu()
 
-    def up_menu(self):
-        """ need text. docstring"""
+    def up_menu(self) -> None:
+        """ Basic widgets. """
         self.master.title("Simply text editor")
         main_menu = Menu(self)
         self.master.config(menu=main_menu)
@@ -36,26 +37,25 @@ class TextEditor(Frame):
         ('All files', '*.*')
     )
 
-    def check_gl_var(self):
-        """ need text. docstring"""
+    def check_gl_var(self) -> bool:
+        """ Verification of the presence of a global variable. """
         if 'filepath_open' in globals():
             return True
         else:
             return False
 
-    def exit_from_editor(self):
-        """ need text. docstring"""
+    def exit_from_editor(self) -> None:
+        """ Exiting the application. """
         self.quit()
 
     def info(self):
-        """ need text. docstring"""
+        """ For the function 'askyesno'. """
         title = "Save"
         massage = "Save the file?"
         return messagebox.askyesno(title, massage)
 
-
-    def select_and_open_file(self):
-        """ need text. docstring"""
+    def select_and_open_file(self) -> None:
+        """ Selecting and opening a file. """
         global filepath_open
         filepath_open = filedialog.askopenfilename(filetypes=filetypes)
         if filepath_open:
@@ -64,16 +64,14 @@ class TextEditor(Frame):
                 txt_notes.delete("1.0", "end-1c")
                 txt_notes.insert("1.0", text)
                 outFile.close()
-        else:
-            pass
 
-    def local(self, filepath_open):
-        """ need text. docstring"""
+    def local(self, filepath_open) -> None:
+        """ To reset the global variable. """
         filepath_open = None
         return filepath_open
 
-    def new_file(self):
-        """ need text. docstring"""
+    def new_file(self) -> None:
+        """ Creating a new text field. """
         gl_check = self.check_gl_var()
         if gl_check:
             global filepath_open
@@ -94,26 +92,21 @@ class TextEditor(Frame):
                     self.save_as_file()
                 txt_notes.delete("1.0", "end-1c")
 
-
-
-
-    def save_as_file(self):
-        """ need text. docstring"""
+    def save_as_file(self) -> None:
+        """ The 'save as...' function. """
         text = txt_notes.get("1.0", "end-1c")
+        global filepath_open
         if text:
-            filepath = filedialog.asksaveasfilename(filetypes=filetypes)
-            if filepath:
-                with open(filepath, "w") as inFile:
+            filepath_open = filedialog.asksaveasfilename(filetypes=filetypes)
+            if filepath_open:
+                with open(filepath_open, "w") as inFile:
                     inFile.write(text)
                     inFile.close()
-        
-        
 
-    def save_file(self):
-        """ need text. docstring"""
+    def save_file(self) -> None:
+        """ The 'save' function."""
         gl_check = self.check_gl_var()
         if gl_check:
-            print(gl_check)
             if filepath_open:
                 text = txt_notes.get("1.0", "end-1c")
                 with open(filepath_open, "w") as outInfile:
@@ -121,10 +114,6 @@ class TextEditor(Frame):
                     outInfile.close()
         elif not gl_check:
             self.save_as_file()
-            print(gl_check)
-
-
-    
 
 
 def main():
