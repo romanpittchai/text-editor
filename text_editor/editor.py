b6353b1f3099c82fe6ed00f823c9a14a9e5cef0e
Pittchai <pittchai@base32>
2023-01-06 20:54:43 +0300

    other func

--- text_editor/editor.py
+++ text_editor/editor.py
@@ -1,4 +1,6 @@
-from tkinter import Frame, Menu, Text, Scrollbar, filedialog, messagebox, Tk
+import webbrowser
+from tkinter import (Frame, Menu, Text, Scrollbar,
+                     filedialog, messagebox, Tk)
 
 
 class TextEditor(Frame):
@@ -12,6 +14,8 @@ class TextEditor(Frame):
         main_menu = Menu(self)
         self.master.config(menu=main_menu)
         file_menu = Menu(main_menu, tearoff=0)
+        file_menu_correction = Menu(main_menu, tearoff=0)
+        file_menu_data = Menu(main_menu, tearoff=0)
         file_menu.add_command(label="New file", command=self.new_file)
         file_menu.add_command(label="Open file",
                               command=self.select_and_open_file)
@@ -20,7 +24,21 @@ class TextEditor(Frame):
         file_menu.add_command(label="Save as", command=self.save_as_file)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.exit_from_editor)
+
+        file_menu_correction.add_command(label="Cut", command=self.cut_text)
+        file_menu_correction.add_command(label="Copy", command=self.copy_text)
+        file_menu_correction.add_separator()
+        file_menu_correction.add_command(label="Paste",
+                                         command=self.paste_text)
+        file_menu_correction.add_command(label="Select text",
+                                         command=self.select_text)
+
+        file_menu_data.add_command(label="Github", command=self.url)
+        file_menu_data.add_command(label="Data", command=self.data)
+
         main_menu.add_cascade(label="File", menu=file_menu)
+        main_menu.add_cascade(label="Сorrection", menu=file_menu_correction)
+        main_menu.add_cascade(label="Data", menu=file_menu_data)
 
         txtFrame = Frame(self.master)
         txtFrame.pack(side="bottom", fill="both", expand=True)
@@ -37,7 +55,7 @@ class TextEditor(Frame):
         ('All files', '*.*')
     )
 
-    def check_gl_var(self) -> bool:
+    def check_gl_var_filepath_open(self) -> bool:
         """ Verification of the presence of a global variable. """
         if 'filepath_open' in globals():
             return True
@@ -104,7 +122,7 @@ class TextEditor(Frame):
                     inFile.close()
 
     def save_file(self) -> None:
-        """ The 'save' function."""
+        """ The 'save' function. """
         gl_check = self.check_gl_var()
         if gl_check:
             if filepath_open:
@@ -114,6 +132,61 @@ class TextEditor(Frame):
                     outInfile.close()
         elif not gl_check:
             self.save_as_file()
+
+    def func_for_copy_cut(self) -> bool:
+        try:
+            global text
+            text = txt_notes.selection_get()
+            global index_first
+            index_first = txt_notes.index("sel.first")
+            global index_last
+            index_last = txt_notes.index("sel.last")
+            txt_notes.selection_clear()
+            text_bool = True
+        except Exception as exc:
+            print(exc)
+            text_bool = False
+        return text_bool
+
+    def cut_text(self) -> None:
+        """ Cut text. """
+        text_bool = self.func_for_copy_cut()
+        if text_bool:
+            txt_notes.delete(index_first, index_last)
+
+    def copy_text(self) -> None:
+        """ Copy text. """
+        self.func_for_copy_cut()
+
+    def paste_text(self):
+        """ Paste text. """
+        index_cursor = txt_notes.index('insert')
+        try:
+            txt_notes.insert(index_cursor, text)
+        except Exception as exc:
+            print(exc)
+            return
+
+    def select_text(self) -> None:
+        """ Select text. """
+        txt_notes.tag_add("sel", "1.0", "end-1c")
+
+    def data(self) -> None:
+        """ Information. """
+        msg = ('This is a simple text editor designed '
+               'to work on texts that do not require any '
+               'specific processing. It was created for '
+               'educational purposes. Completely written in '
+               'python3 using the tkinter library. '
+               'Perhaps it will be finalized over time. '
+               'The project can be used and refined by '
+               'other people, both for educational and '
+               'practical purposes.')
+        messagebox.showinfo("Information", msg)
+
+    def url(self) -> None:
+        """ For git url. """
+        webbrowser.open("https://github.com/romanpittchai/text-editor")
 
 
 def main():
